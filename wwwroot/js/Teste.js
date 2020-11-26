210f737f7c80a8c79a278c5a93b62f66d9f5865b
AlexSouza87 <61298270+AlexSouza87@users.noreply.github.com>
2020-11-26 01:10:40 -0300

    Ajustes na IHC das telas TCCs e Calendarios do perfil de coordenador

---
+++ wwwroot/js/Teste.js
@@ -0,0 +1,11 @@
+﻿showInPopUp = (url, title) => {
+    $.ajax({
+        type:"GET",
+        url: url,
+        success: function (res) {
+            $("#janela-modal .modal-body").html(res);
+            $("#janela-modal .modal-title").html(title);
+            $("#janela-modal").modal('show');
+        }
+    })
+}
