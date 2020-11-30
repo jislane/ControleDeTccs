d7e9b5e14998c72a3f264a8d7ec4df4d2f15c0e0
Willian Farias <williansantos_141@hotmail.com>
2020-11-30 20:11:38 -0300

    Gera relatório PDF para divulgação de TCC homologado banca

---
+++ Areas/Coordenador/Controllers/RelatorioAtividades.cs
@@ -0,0 +1,103 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SistemaDeControleDeTCCs.Models;
+
+namespace SistemaDeControleDeTCCs.Controllers
+{
+    public class RelatorioAtividades : Report
+    {
+        public RelatorioAtividades()
+        {
+            Paisagem = false;
+        }
+
+        public override void MontaCorpoDados(List<Tcc> tccs, List<Banca> bancas, string semestre)
+        {
+            base.MontaCorpoDados(tccs, bancas, semestre);
+
+            BaseColor preto = new BaseColor(0, 0, 0);
+            BaseColor fundo = new BaseColor(200, 200, 200);
+            Font font = FontFactory.GetFont("Verdana", 8, Font.NORMAL, preto);
+            Font titulo = FontFactory.GetFont("Verdana", 8, Font.BOLD, preto);
+            Font titulo2 = FontFactory.GetFont("Verdana", 12, Font.BOLD, preto);
+
+            PdfPTable micros = new PdfPTable(1);
+            micros.TotalWidth = doc.PageSize.Width - (doc.LeftMargin + doc.RightMargin);
+            PdfPCell cell2 = new PdfPCell(new Phrase("BANCAS DE TCC " + semestre, titulo2));
+            cell2.Border = 0;
+            cell2.HorizontalAlignment = Element.ALIGN_CENTER;
+            cell2.PaddingBottom = 20f;
+            cell2.PaddingTop = 30f;
+            micros.AddCell(cell2);
+
+            doc.Add(micros);
+
+            if(tccs != null && tccs.Count > 0)
+            {
+                foreach (Tcc tcc in tccs)
+                {
+                    PdfPTable dadosDiscente = new PdfPTable(1);
+                    dadosDiscente.WidthPercentage = 100f;
+                    float[] sizes = new float[] { 12f };
+                    dadosDiscente.SetWidths(sizes);
+                    dadosDiscente.HorizontalAlignment = Element.ALIGN_LEFT;
+
+                    cell2 = new PdfPCell(new Phrase("DISCENTE: " + tcc.Usuario.Nome.ToUpper() + " " + tcc.Usuario.Sobrenome.ToUpper(), font));
+                    cell2.Colspan = 2;
+                    cell2.Border = 0;
+                    cell2.HorizontalAlignment = Element.ALIGN_LEFT;
+                    dadosDiscente.AddCell(cell2);
+
+                    cell2 = new PdfPCell(new Phrase("ORIENTADOR(A): " + bancas.Where(x => x.TccId == tcc.TccId).FirstOrDefault().Usuario.Nome.ToUpper() + " " + bancas.Where(x => x.TccId == tcc.TccId).FirstOrDefault().Usuario.Sobrenome.ToUpper(), font));
+                    cell2.Border = 0;
+                    cell2.HorizontalAlignment = Element.ALIGN_LEFT;
+                    dadosDiscente.AddCell(cell2);
+
+                    cell2 = new PdfPCell(new Phrase("DATA DA APRESENTAÇÃO: " + tcc.DataApresentacao.Value.ToString("dd/MM/yyyy HH:mm"), font));
+                    cell2.Colspan = 2;
+                    cell2.Border = 0;
+                    cell2.HorizontalAlignment = Element.ALIGN_LEFT;
+                    dadosDiscente.AddCell(cell2);
+
+                    cell2 = new PdfPCell(new Phrase("LOCAL DE APRESENTAÇÃO: " + tcc.LocalApresentacao, font));
+                    cell2.Border = 0;
+                    cell2.HorizontalAlignment = Element.ALIGN_LEFT;
+                    dadosDiscente.AddCell(cell2);
+
+                    cell2 = new PdfPCell(new Phrase("TEMA: " + tcc.Tema, font));
+                    cell2.Border = 0;
+                    cell2.Colspan = 2;
+                    cell2.HorizontalAlignment = Element.ALIGN_LEFT;
+                    dadosDiscente.AddCell(cell2);
+
+                    cell2 = new PdfPCell(new Phrase("RESUMO: " + tcc.Resumo, font));
+                    cell2.Border = 0;
+                    cell2.HorizontalAlignment = Element.ALIGN_LEFT;
+                    cell2.PaddingBottom = 10f;
+                    dadosDiscente.AddCell(cell2);
+
+                    doc.Add(dadosDiscente);
+                }
+            }
+            else
+            {
+                PdfPTable dadosDiscente = new PdfPTable(1);
+                dadosDiscente.WidthPercentage = 100f;
+                float[] sizes = new float[] { 12f };
+                dadosDiscente.SetWidths(sizes);
+                dadosDiscente.HorizontalAlignment = Element.ALIGN_LEFT;
+
+                cell2 = new PdfPCell(new Phrase("Nenhuma banca de TCC encontrada", font));
+                cell2.Colspan = 2;
+                cell2.Border = 0;
+                cell2.HorizontalAlignment = Element.ALIGN_LEFT;
+                dadosDiscente.AddCell(cell2);
+
+                doc.Add(dadosDiscente);
+            }
+        }
+    }
+}
---
+++ Areas/Coordenador/Controllers/RelatoriosController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SistemaDeControleDeTCCs.Controllers;
+using SistemaDeControleDeTCCs.Data;
+using SistemaDeControleDeTCCs.Models;
+
+namespace SistemaDeControleDeTCCs.Controllers
+{
+    [Area("Coordenador")]
+    [Authorize(Roles = "Coordenador")]
+    public class RelatoriosController : Controller
+    {
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        private readonly SistemaDeControleDeTCCsContext _context;
+
+        public RelatoriosController(SistemaDeControleDeTCCsContext context, IWebHostEnvironment hostingEnvironment)
+        {
+            _context = context;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            var calendarios = _context.Calendario.Select(x => new { Value = x.CalendarioId, Text = string.Format("{0}.{1}", x.Ano, x.Semestre) }).ToList();
+            //calendarios.Add(new { Value = -1, Text = "Sem data" });
+            ViewBag.Semestre = new SelectList(calendarios.OrderByDescending(x => x.Text), "Value", "Text");
+            return View();
+        }
+
+        private RelatorioAtividades getRelatorio()
+        {
+            var rpt = new RelatorioAtividades();
+            string contentRootPath = _hostingEnvironment.ContentRootPath;
+            rpt.BasePath = contentRootPath;
+
+            rpt.PageTitle = "SISTEMA DE CONTROLE DE TCC";
+            rpt.PageSubTitle = "Coordenação do Bacharelado de Sistemas de Informação - CBSI";
+            rpt.ImprimirCabecalhoPadrao = true;
+            rpt.ImprimirRodapePadrao = true;
+
+            return rpt;
+        }
+
+        public ActionResult Preview(int filterSemestre)
+        {
+            RelatorioAtividades rpt = getRelatorio();
+            string semestre = "";
+            List<Tcc> tccs = null;
+            List<Banca> bancas = null;
+
+            if (filterSemestre > 0)
+            {
+                tccs = _context.Tccs.Where(x => x.StatusId == 3).ToList();
+                bancas = new List<Banca>();
+                foreach (Tcc tcc in tccs)
+                {
+                    tcc.Usuario = _context.Usuario.Where(x => x.Id.Equals(tcc.UsuarioId)).FirstOrDefault();
+                    Banca banca = _context.Banca.Where(x => x.TccId == tcc.TccId && x.TipoUsuarioId == 7).FirstOrDefault();
+                    banca.Usuario = _context.Usuario.Where(x => x.Id.Equals(banca.UsuarioId)).FirstOrDefault();
+                    bancas.Add(banca);
+                }
+                Calendario calendario = _context.Calendario.Where(x => x.CalendarioId == filterSemestre).FirstOrDefault();
+                if(calendario != null)
+                    semestre = calendario.Ano + "." + calendario.Semestre;
+                tccs = tccs.Where(x => x.DataApresentacao >= calendario.DataInicio && x.DataApresentacao <= calendario.DataFim).ToList();
+                var calendarios = _context.Calendario.Select(x => new { Value = x.CalendarioId, Text = string.Format("{0}.{1}", x.Ano, x.Semestre) }).ToList();
+                //calendarios.Add(new { Value = -1, Text = "Sem data" });
+                ViewBag.Semestre = new SelectList(calendarios.OrderByDescending(x => x.Text), "Value", "Text", filterSemestre);
+            }
+            else if (filterSemestre == -1)
+            {
+                tccs = tccs.Where(x => x.DataApresentacao == null).ToList();
+                var calendarios = _context.Calendario.Select(x => new { Value = x.CalendarioId, Text = string.Format("{0}.{1}", x.Ano, x.Semestre) }).ToList();
+                //calendarios.Add(new { Value = -1, Text = "Sem data" });
+                ViewBag.Semestre = new SelectList(calendarios.OrderByDescending(x => x.Text), "Value", "Text", filterSemestre);
+            }
+            else
+            {
+                var calendarios = _context.Calendario.Select(x => new { Value = x.CalendarioId, Text = string.Format("{0}.{1}", x.Ano, x.Semestre) }).ToList();
+                //calendarios.Add(new { Value = -1, Text = "Sem data" });
+                ViewBag.Semestre = new SelectList(calendarios.OrderByDescending(x => x.Text), "Value", "Text");
+            }
+
+            return File(rpt.GetOutput(tccs, bancas, semestre).GetBuffer(), "application/pdf");
+        }
+    }
+}
