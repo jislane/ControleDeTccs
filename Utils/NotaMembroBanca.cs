f91011dbb7ce8a7a5462507e1c079bc6c9786475
Arielson <40209441+ArielsonRibeiro@users.noreply.github.com>
2021-03-21 17:07:47 -0300

    Correção das Notas dos TCCs.

---
+++ Utils/NotaMembroBanca.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaDeControleDeTCCs.Utils
+{
+    public class NotaMembroBanca
+    {
+
+        public String MembroBanca { get; set; }
+        public double? Nota { get; set; }
+    }
+}
