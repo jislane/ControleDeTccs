3d83d79207589fb69fd6b388957e69f32f1229db
Arielson <40209441+ArielsonRibeiro@users.noreply.github.com>
2021-03-22 16:11:05 -0300

    Correção da Tela login podendo ser aberta mesmo com o usuário já logado.

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -59,6 +59,7 @@ namespace SistemaDeControleDeTCCs.Areas.Identity.Pages.Account
 
         public async Task OnGetAsync(string returnUrl = null)
         {
+
             if (!string.IsNullOrEmpty(ErrorMessage))
             {
                 ModelState.AddModelError(string.Empty, ErrorMessage);
